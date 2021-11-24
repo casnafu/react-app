0cf99f319ed64166e949b7542508ba652d87154b
casnafu <camiferreir@gmail.com>
2021-11-24 23:31:43 +0100

    .

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="container">
         <Search mainCity="Bremen" />
         <div className="link">
-          Project created by <a className="text-decoration-none" href="https://camif-me.netlify.app/" target="_blank" rel="noreferrer" title="Portfolio">Camilla Ferreira</a> and its open source code is on <a className="text-decoration-none" href="https://github.com/casnafu/react-app" rel="noreferrer" target="_blank" title="GitHub">GitHub</a> avaliable.
+          Project created by <a className="text-decoration-none" href="https://camif-me.netlify.app/" target="_blank" rel="noreferrer" title="Portfolio">Camilla Ferreira</a> and it is avaliable on <a className="text-decoration-none" href="https://github.com/casnafu/react-app" rel="noreferrer" target="_blank" title="GitHub">GitHub</a>.
         </div>
       </div>
     </div>
